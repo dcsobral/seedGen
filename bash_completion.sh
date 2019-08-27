88da8bc328899922cee57af3d52d617eb0e1014c
Daniel Sobral <dcsobral@gmail.com>
2019-08-27 12:30:37 -0600

    Add completion for sortByGroup and uniquePrefabs

--- bash_completion.sh
+++ bash_completion.sh
@@ -1,9 +1,16 @@
-_seedExtract ()
-{
+_seedExtract() {
     COMPREPLY=($(compgen -f -X '!*.zip' | sed -nr '/-'"$2"'[0-9]+.zip/ s/.*-([0-9]+).zip/\1/p'))
 }
-complete -o dirnames -o filenames -f -X '!*.xml' uniquePrefabs.sh
+
+_prefabGroups() {
+	: "${F7D2D:=/mnt/c/Users/Daniel/Desktop/RH5.5Server}"
+
+	COMPREPLY=( $(xmlstarlet sel -t -m "//prefab_rule[starts-with(@name,'$2')][prefab[@name]]" -v @name -n "${F7D2D}/Data/Config/rwgmixer.xml") )
+}
+
 complete -F _seedExtract extractPrefabs.sh
+complete -F _prefabGroups sortByGroup.sh
+complete -o dirnames -o filenames -f -X '!*.xml' uniquePrefabs.sh
 complete -o dirnames -o filenames -f -X '!*.zip' extract.sh
 complete -o dirnames -o filenames -f -X '!*.xml' prefabGroups.sh
 complete -o dirnames -o filenames -f -X '!*.xml' missingPrefabs.sh
