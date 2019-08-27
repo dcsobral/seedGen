e372c40619f6666f6bb0bf21bf465ebbecce0f9f
Daniel Sobral <dcsobral@gmail.com>
2019-08-27 17:07:35 -0600

    Make extractPrefabs accept multiple sizes

--- extractPrefabs.sh
+++ extractPrefabs.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 1 ]]; then
+if [[ $# -lt 1 ]]; then
 	echo >&2 "$0 <size>"
 	exit 1
 fi
 
-SIZE="$1"
+for SIZE; do
+	echo "Extracting ${SIZE}"
+	mkdir -p "${SIZE}"
 
-mkdir -p "${SIZE}"
-
-for map in *"${SIZE}.zip"; do
-	[[ -f ${SIZE}/${map%.zip}.xml ]] || unzip "$map" "${map%.zip}.xml" -d "${SIZE}"
+	for map in *"${SIZE}.zip"; do
+		[[ -f ${SIZE}/${map%.zip}.xml ]] || unzip "$map" "${map%.zip}.xml" -d "${SIZE}"
+	done
 done
 
