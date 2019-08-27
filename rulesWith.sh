fbf1b5f721975d1aecb3f45d445b4c8a04b5c9e6
Daniel Sobral <dcsobral@gmail.com>
2019-08-27 16:47:28 -0600

    Improve output of rulesWith

--- rulesWith.sh
+++ rulesWith.sh
@@ -5,11 +5,18 @@ IFS=$'\n\t'
 if [[ $# -eq 0 ]]; then
 	echo >&2 "$0 <prefab_name>"
 	exit 1
+elif [[ $# -gt 1 ]]; then
+	PRECMD='echo "$prefab:"'
+	PREFIX=$'\t'
+else
+	PRECMD=':'
+	PREFIX=''
 fi
 
 : "${F7D2D:=/mnt/c/Users/Daniel/Desktop/RH5.5Server}"
 
 for prefab; do
-	xmlstarlet sel -t -m "//prefab_rule[prefab[starts-with(@name,'$prefab')]]" -v @name -n "${F7D2D}/Data/Config/rwgmixer.xml"
+	eval "$PRECMD"
+	xmlstarlet sel -t -m "//prefab_rule[prefab[starts-with(@name,'$prefab')]]" -o "$PREFIX" -v @name -n "${F7D2D}/Data/Config/rwgmixer.xml"
 done
 
