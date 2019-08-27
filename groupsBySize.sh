235d2fc87e361f7a62038f7dfc7a847dde5837c4
Daniel Sobral <dcsobral@gmail.com>
2019-08-27 14:07:45 -0600

    Add groupsBySize.sh

---
+++ groupsBySize.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\t\n'
+
+BIN="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+: "${F7D2D:=/mnt/c/Users/Daniel/Desktop/RH5.5Server}"
+PREFABS="${F7D2D}/Data/Prefabs"
+
+prefabRules() {
+	xmlstarlet sel -t -m "//prefab_rule[prefab[@name]]" -v @name -n "${F7D2D}/Data/Config/rwgmixer.xml"
+}
+
+prefabs() {
+	xmlstarlet sel -t -m "//prefab_rule[@name='$1']/prefab[@name]" -v @name -n "${F7D2D}/Data/Config/rwgmixer.xml"
+}
+
+declare -a RULES
+declare -A DIM
+
+mapfile -t RULES < <(prefabRules)
+for rule in "${RULES[@]}"; do
+	prefabs=( $(prefabs "$rule") )
+	echo "$rule"
+	for prefab in "${prefabs[@]}"; do
+		if [[ -n ${DIM["$prefab"]+abc} ]]; then
+			IFS=',' read x y z <<<"${DIM["$prefab"]}"
+		else
+			IFS=',' read x y z < <("${BIN}/prefabSize.sh" "${PREFABS}/${prefab}.tts")
+			DIM["$prefab"]="$x,$y,$z"
+		fi
+		area=$((x * z))
+		printf "\t%d\t%s\t%s\n" $area "$x,$y,$z" "$prefab"
+	done | sort -n
+done
+
