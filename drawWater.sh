e62c362307b1694db6e484e3a516c85cbdc32c26
Daniel Sobral <dcsobral@gmail.com>
2019-09-19 15:39:13 -0600

    Working version of drawWater

--- drawWater.sh
+++ drawWater.sh
@@ -15,26 +15,56 @@ IMG="$2"
 SIZE="$3"
 DIM="${SIZE}x${SIZE}"
 CENTER="$((SIZE / 2))"
+LAKEIMG="lakes-${IMG}"
+TMPIMG="tmp=${IMG}"
 
 # sel -t -m "//Water" --sort a:n:- "str:tokenize(@pos, ',')[2]" -v @pos -n
-mapfile -t < <(sel -t -m "//Water" -v @pos -n "$XML" )
+mapfile -t < <(xmlstarlet sel -t -m "//Water" --sort a:n:- "str:tokenize(@pos, ',')[2]" -v @pos -n "$XML" )
 
-for coord in "${MAPFILE[@]}"; do
-	IFS=", " read x y z <<<"${coord}"
-	x=$((x+CENTER))
-	z=$((z+CENTER))
-	echo "$coord"; convert -size "$DIM" -depth 16 "gray:dtm.raw" -format "%[fx:256*s.p{$x,$z}]\n" info:
+cp "${IMG}" "${LAKEIMG}"
+INDEX=0
+while [[ $INDEX -lt ${#MAPFILE[@]} ]]; do
+	IFS=", " read x y z <<<"${MAPFILE["$INDEX"]}"
+	pixelLevel=$(((y + 1) * 256))
+	convert -size "${DIM}" -depth 16 gray:dtm.raw -flip -threshold $pixelLevel watermask.png
+	rm -f water.txt
+	while [[ $INDEX -lt ${#MAPFILE[@]} && ${MAPFILE["$INDEX"]} == *,$y,* ]]; do
+		IFS=", " read x _ignored z <<<"${MAPFILE["$INDEX"]}"
+		x=$((x+CENTER))
+		z=$((CENTER-z))
+		level=$(convert -size "$DIM" -depth 16 "gray:dtm.raw" -flip \
+				-format "%[fx:floor(256*s.p{$x,$z})]\n" info:)
+		if [[ $level -le $pixelLevel ]]; then
+			echo "color $x,$z floodfill" >> water.txt
+		fi
+		INDEX=$((INDEX + 1))
+	done
+	convert watermask.png \
+		-alpha on \
+		-fill blue \
+		-draw "@water.txt" \
+		-fill black \
+		-opaque white \
+		-transparent black lakes.png
+	convert "${LAKEIMG}" lakes.png -composite "${TMPIMG}"
+	mv "${TMPIMG}" "${LAKEIMG}"
 done
 
-rm -f water.txt; for coord in "${MAPFILE[@]}"; do
-	IFS=", " read x y z <<<"${coord}"
-	x=$((x+CENTER))
-	z=$((CENTER-z))
-	echo "color $x,$z floodfill" >> water.txt
-done
-
-convert -size "${DIM}" -depth 16 gray:dtm.raw -flip -threshold $((0x2600)) watermask.png
-convert watermask.png -alpha on -fill blue -draw "@water.txt" -fill black -opaque white -transparent black lakes.png
-convert "${IMG}" lakes.png -composite "lakes-${IMG}"
-
+#for coord in "${MAPFILE[@]}"; do
+#	IFS=", " read x y z <<<"${coord}"
+#	x=$((x+CENTER))
+#	z=$((z+CENTER))
+#	echo "$coord"; convert -size "$DIM" -depth 16 "gray:dtm.raw" -format "%[fx:256*s.p{$x,$z}]\n" info:
+#done
+#
+#rm -f water.txt; for coord in "${MAPFILE[@]}"; do
+#	IFS=", " read x y z <<<"${coord}"
+#	x=$((x+CENTER))
+#	z=$((CENTER-z))
+#	echo "color $x,$z floodfill" >> water.txt
+#done
+#
+#convert -size "${DIM}" -depth 16 gray:dtm.raw -flip -threshold $((0x2600)) watermask.png
+#convert watermask.png -alpha on -fill blue -draw "@water.txt" -fill black -opaque white -transparent black lakes.png
+#convert "${IMG}" lakes.png -composite "lakes-${IMG}"
 
