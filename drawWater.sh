c07d02a82b4484772f646a0bdb46e575e1430eac
Daniel Sobral <dcsobral@gmail.com>
2019-09-25 15:55:51 -0600

    Improve water drawing

--- drawWater.sh
+++ drawWater.sh
@@ -3,8 +3,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
-: "${F7D2D:=/mnt/c/Users/Daniel/Desktop/RH5.5Server}"
-
 if [[ $# -ne 3 ]]; then
         echo >&2 "$0 <water_info.xml> <image> <size>"
         exit 1
@@ -25,7 +23,7 @@ cp "${IMG}" "${LAKEIMG}"
 INDEX=0
 while [[ $INDEX -lt ${#MAPFILE[@]} ]]; do
 	IFS=", " read x y z <<<"${MAPFILE["$INDEX"]}"
-	pixelLevel=$(((y + 1) * 256))
+	pixelLevel=$((y * 256 + 129))
 	convert -size "${DIM}" -depth 16 gray:dtm.raw -flip -threshold $pixelLevel watermask.png
 	rm -f water.txt
 	while [[ $INDEX -lt ${#MAPFILE[@]} && ${MAPFILE["$INDEX"]} == *,$y,* ]]; do
@@ -49,22 +47,4 @@ while [[ $INDEX -lt ${#MAPFILE[@]} ]]; do
 	convert "${LAKEIMG}" lakes.png -composite "${TMPIMG}"
 	mv "${TMPIMG}" "${LAKEIMG}"
 done
-
-#for coord in "${MAPFILE[@]}"; do
-#	IFS=", " read x y z <<<"${coord}"
-#	x=$((x+CENTER))
-#	z=$((z+CENTER))
-#	echo "$coord"; convert -size "$DIM" -depth 16 "gray:dtm.raw" -format "%[fx:256*s.p{$x,$z}]\n" info:
-#done
-#
-#rm -f water.txt; for coord in "${MAPFILE[@]}"; do
-#	IFS=", " read x y z <<<"${coord}"
-#	x=$((x+CENTER))
-#	z=$((CENTER-z))
-#	echo "color $x,$z floodfill" >> water.txt
-#done
-#
-#convert -size "${DIM}" -depth 16 gray:dtm.raw -flip -threshold $((0x2600)) watermask.png
-#convert watermask.png -alpha on -fill blue -draw "@water.txt" -fill black -opaque white -transparent black lakes.png
-#convert "${IMG}" lakes.png -composite "lakes-${IMG}"
 
