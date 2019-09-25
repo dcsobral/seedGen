45c4f61163dc5161091755512c3794071b1b9a0c
Daniel Sobral <dcsobral@gmail.com>
2019-09-25 16:36:27 -0600

    Fix level query, and print error messages on bad water

--- drawWater.sh
+++ drawWater.sh
@@ -16,24 +16,26 @@ CENTER="$((SIZE / 2))"
 LAKEIMG="lakes-${IMG}"
 TMPIMG="tmp=${IMG}"
 
-# sel -t -m "//Water" --sort a:n:- "str:tokenize(@pos, ',')[2]" -v @pos -n
 mapfile -t < <(xmlstarlet sel -t -m "//Water" --sort a:n:- "str:tokenize(@pos, ',')[2]" -v @pos -n "$XML" )
 
 cp "${IMG}" "${LAKEIMG}"
 INDEX=0
 while [[ $INDEX -lt ${#MAPFILE[@]} ]]; do
 	IFS=", " read x y z <<<"${MAPFILE["$INDEX"]}"
-	pixelLevel=$((y * 256 + 129))
+	pixelLevel=$((y * 256 + 128))
 	convert -size "${DIM}" -depth 16 gray:dtm.raw -flip -threshold $pixelLevel watermask.png
 	rm -f water.txt
 	while [[ $INDEX -lt ${#MAPFILE[@]} && ${MAPFILE["$INDEX"]} == *,$y,* ]]; do
 		IFS=", " read x _ignored z <<<"${MAPFILE["$INDEX"]}"
-		x=$((x+CENTER))
-		z=$((CENTER-z))
+		xabs=$((x+CENTER))
+		zabs=$((CENTER-z))
 		level=$(convert -size "$DIM" -depth 16 "gray:dtm.raw" -flip \
-				-format "%[fx:floor(256*s.p{$x,$z})]\n" info:)
+				-format "%[fx:round(65536*s.p{$xabs,$zabs})]\n" info:)
 		if [[ $level -le $pixelLevel ]]; then
-			echo "color $x,$z floodfill" >> water.txt
+			echo "color $xabs,$zabs floodfill" >> water.txt
+		else
+			printf >&2 "Requested level %04x greater or equal than %04x at %d,%d" \
+				"$level" "$pixelLevel" "$x" "$z"
 		fi
 		INDEX=$((INDEX + 1))
 	done
