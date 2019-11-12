e7c36981531c5f92d9ee8b5e8406d3a3be53e43b
Daniel Sobral <dcsobral@gmail.com>
2019-11-12 14:06:36 -0700

    Use full path for log file

--- startServer.sh
+++ startServer.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 IFS=$'\t\n'
 
-: "${F7D2D:=/mnt/c/Users/Daniel/Desktop/RH5.5Server}"
+: "${F7D2D:?Please export F7D2D with 7D2D install folder}"
+: "${LOG:=${F7D2D}/log.txt}"
 
 if [[ $# -ne 2 ]]; then
 	echo >&2 "$0 <size> <seed>"
@@ -14,8 +15,8 @@ SEED="${2}"
 
 cd "${F7D2D}"
 
-rm -f log.txt || { sleep 30; rm -f log.txt; }
+rm -f "${LOG}" || { sleep 30; rm -f "${LOG}"; }
 rm -fr UserData/
 
-./7DaysToDieServer.exe -quit -batchmode -nographics -logfile log.txt -configfile=serverconfig.xml -UserDataFolder=UserData -GameWorld=RWG -WorldGenSize="${SIZE}" -WorldGenSeed="${SEED}" -GameName=test -verbose -dedicated &
+./7DaysToDieServer.exe -quit -batchmode -nographics -logfile "$(wslpath -w "${LOG}")" -configfile=serverconfig.xml -UserDataFolder=UserData -GameWorld=RWG -WorldGenSize="${SIZE}" -WorldGenSeed="${SEED}" -GameName=test -verbose -dedicated &
 
