3ea3fd2984b134e542a5077f07a457e816f22972
Daniel Sobral <dcsobral@gmail.com>
2019-11-13 12:31:30 -0700

    Use F7D2D in missingPrefabs

--- missingPrefabs.sh
+++ missingPrefabs.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+
+: "${F7D2D:?Please export F7D2D with 7D2D install folder}"
+
 if [[ $# -ne 1 ]]; then
 	echo >&2 "$0 <prefab.xml>"
 	exit 1
 fi
 
-xmlstarlet sel -t -m / --var "p=document('$1')" -m "//prefab_rule[prefab[@name]]" --if "prefab[not(@name=\$p//decoration/@name)]" -v @name -n -b -m "prefab[not(@name=\$p//decoration/@name)]" --sort a:t:u @name -o "  " -v '@name' --var "x=document(concat('../../Data/Prefabs/',@name,'.xml'))" -o " (" -v "\$x/prefab/property[@name='Zoning']/@value" -o ")" -n ../../Data/Config/rwgmixer.xml
+# 7d2d prefabs xml files come with "bom", which prevents the file from being read with "document"
+# use the following to remove bom from files, if needed
+# sed -i 's/\xef\xbb\xbf//' $filename
+
+xmlstarlet sel -t -m / --var "p=document('$1')" -m "//prefab_rule[prefab[@name]]" --if "prefab[not(@name=\$p//decoration/@name)]" -v @name -n -b -m "prefab[not(@name=\$p//decoration/@name)]" --sort a:t:u @name -o "  " -v '@name' --var "x=document(concat('${F7D2D}/Data/Prefabs/',@name,'.xml'))" -o " (" -v "\$x/prefab/property[@name='Zoning']/@value" -o ")" -n "${F7D2D}/Data/Config/rwgmixer.xml"
 
