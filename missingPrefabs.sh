5e60440042f51a97df4530cc87f0dd0d8c79db7f
Daniel Sobral <dcsobral@gmail.com>
2019-12-01 16:49:25 -0700

    Ignore group rules when displaying missing prefabs

--- missingPrefabs.sh
+++ missingPrefabs.sh
@@ -12,5 +12,5 @@ fi
 # use the following to remove bom from files, if needed
 # sed -i 's/\xef\xbb\xbf//' $filename
 
-xmlstarlet sel -t -m / --var "p=document('$1')" -m "//prefab_rule[prefab[@name]]" --if "prefab[not(@name=\$p//decoration/@name)]" -v @name -n -b -m "prefab[not(@name=\$p//decoration/@name)]" --sort a:t:u @name -o "  " -v '@name' --var "x=document(concat('${F7D2D}/Data/Prefabs/',@name,'.xml'))" -o " (" -v "\$x/prefab/property[@name='Zoning']/@value" -o ")" -n "${F7D2D}/Data/Config/rwgmixer.xml"
+xmlstarlet sel -t -m / --var "p=document('$1')" -m "//prefab_rule[prefab[@name]]" --if "prefab[not(@name=\$p//decoration/@name)]" -v @name -n -b -m "prefab[@name][not(@name=\$p//decoration/@name)]" --sort a:t:u @name -o "  " -v '@name' --var "x=document(concat('${F7D2D}/Data/Prefabs/',@name,'.xml'))" -o " (" -v "\$x/prefab/property[@name='Zoning']/@value" -o ")" -n "${F7D2D}/Data/Config/rwgmixer.xml"
 
