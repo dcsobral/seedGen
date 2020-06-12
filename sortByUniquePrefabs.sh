d3066d82b39d6b21181161c2239a2d2bc0ae5edc
Daniel Sobral <dcsobral@gmail.com>
2020-06-12 09:33:54 -0600

    Avoid errors when filenames start with dashes

--- sortByUniquePrefabs.sh
+++ sortByUniquePrefabs.sh
@@ -2,7 +2,7 @@
 
 for file in *.xml; do
 	printf "%4d %s\n" \
-		"$(grep decoration "$file" | cut -s -d '"' -f 4 | sort -u | wc -l)" \
+		"$(grep decoration -- "$file" | cut -s -d '"' -f 4 | sort -u | wc -l)" \
 		"${file%.xml}"
 done | sort -n
 
