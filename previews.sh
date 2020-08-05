dbfbccf87d3804ea27d696ae9394c31c947ba84e
Daniel Sobral <dcsobral@gmail.com>
2020-08-05 16:55:57 -0600

    Fix impossible argument detected by bash lint mode

--- previews.sh
+++ previews.sh
@@ -7,7 +7,7 @@ cd "${F7D2D}/previews"
 if [[ $# -lt 1 ]]; then
 	# word splitting is intended
 	# shellcheck disable=SC2046
-	set -- $(compgen -f -X '!*.zip' | sed -nr '/-'"$2"'[0-9]+.zip/ s/.*-([0-9]+).zip/\1/p' | sort -u)
+	set -- $(compgen -f -X '!*.zip' | sed -nr '/-[0-9]+.zip/ s/.*-([0-9]+).zip/\1/p' | sort -u)
 fi
 
 for SIZE; do
--- spawnpoints.sh
+++ spawnpoints.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\t\n'
 
-pushd "${F7D2D}/previews"
+cd "${F7D2D}/previews"
 
 if [[ $# -lt 1 ]]; then
 	# word splitting is intended
 	# shellcheck disable=SC2046
-	set -- $(compgen -f -X '!*.zip' | sed -nr '/-'"$2"'[0-9]+.zip/ s/.*-([0-9]+).zip/\1/p' | sort -u)
+	set -- $(compgen -f -X '!*.zip' | sed -nr '/-[0-9]+.zip/ s/.*-([0-9]+).zip/\1/p' | sort -u)
 fi
 
 for SIZE; do
@@ -16,6 +18,4 @@ for SIZE; do
 		[[ -f ${SIZE}-spawnpoints/${map%.zip}-spawnpoints.xml ]] || unzip "$map" "${map%.zip}-spawnpoints.xml" -d "${SIZE}-spawnpoints"
 	done
 done
-
-popd
 
