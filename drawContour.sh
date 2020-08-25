88dcb0bd50b3e75d2c22d3d8fb4b70cbc2b61760
Daniel Sobral <dcsobral@gmail.com>
2020-08-25 14:05:28 -0600

    Keep original white lines contour

--- drawContour.sh
+++ drawContour.sh
@@ -16,20 +16,21 @@ CONTOUR="${NAME}-contour.png"
 [[ ! -f dtm.png ]] && convert -size "${IMGSIZE}" -depth 16 gray:dtm.raw -flip dtm.png
 
 # Contour lines
-convert biomes.png \
-	-fill '#949442' -opaque '#ffa800' \
-	-fill '#393931' -opaque '#ba00ff' \
-        -threshold 50% bw.png
+#convert biomes.png \
+#	-fill '#949442' -opaque '#ffa800' \
+#	-fill '#393931' -opaque '#ba00ff' \
+#        -threshold 50% bw.png
 convert dtm.png -depth 8 \
 	\( -size 1x500 gradient: -rotate 90 -duplicate 49 +append +repage \) \
 	-clut -morphology edgein diamond:1 -threshold 40% contour.png
-convert contour.png bw.png -compose Plus -composite \
-	\( bw.png -negate \) -compose Multiply -composite \
-	-transparent black plus.png
-convert contour.png bw.png -compose Subtract -composite -negate \
-	bw.png -compose Multiply -composite \
-	-negate -transparent white sub.png
-convert plus.png sub.png -composite "${CONTOUR}"
+#convert contour.png bw.png -compose Plus -composite \
+#	\( bw.png -negate \) -compose Multiply -composite \
+#	-transparent black plus.png
+#convert contour.png bw.png -compose Subtract -composite -negate \
+#	bw.png -compose Multiply -composite \
+#	-negate -transparent white sub.png
+#convert plus.png sub.png -composite "${CONTOUR}"
+convert contour.png -transparent black "${CONTOUR}"
 
 echo "${CONTOUR}"
 
