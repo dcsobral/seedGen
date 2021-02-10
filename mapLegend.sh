5abac19fc0308e6025eef952e1afca000775a418
Daniel Sobral <dcsobral@gmail.com>
2021-02-10 13:48:16 -0700

    Long overdue update to the snow color on the legend

--- mapLegend.sh
+++ mapLegend.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+OUT="$PWD/legend.png"
+
 montage -title 'Map Preview Legend' -pointsize 16 \
 	-label Trader xc:magenta \
 	-label Crop xc:olive \
@@ -18,12 +20,13 @@ montage -title 'Map Preview Legend' -pointsize 16 \
 	-label 'Burnt Forest' 'xc:#393931' \
 	-label Forest 'xc:#004000' \
 	-label Desert 'xc:#FFE477' \
-	-label Snow xc:white \
+	-label Snow 'xc:#c3c4d9' \
 	-label Water 'xc:#738cce' \
 	-label 'Asphalt Road' 'xc:#ceb584' \
 	-label 'Gravel Road' 'xc:#9c8c7b' \
 	-label Radiation 'xc:rgb(255,0,0)' \
 	-shadow -geometry 128x128+5+5 \
-	legend.png
+	"${OUT}"
 
-echo "$PWD/legend.png"
+echo "${OUT}"
+
