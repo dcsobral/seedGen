a9291b39b37cc5bf2454b22c20ee46837038b388
Daniel Sobral <dcsobral@gmail.com>
2021-05-19 12:20:10 -0600

    allSpecials now prints a lot of different categories

--- allSpecials.sh
+++ allSpecials.sh
@@ -6,36 +6,29 @@ BIN="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 #mapfile -t SPECIALS < <(ls ${SPECIAL_FOLDER})
 
 getSorts() {
-#	declare -a SPECIAL_FILES
-#	for special in "${SPECIALS[@]}"; do
-#		if [[ -f "${SPECIAL_FOLDER}/$special" ]]; then
-#			echo -n "${special%.txt}"$'\t'
-#			SPECIAL_FILES+=( "${SPECIAL_FOLDER}/$special" )
-#		fi
-#	done
-#	echo
 
-	echo $'         top7\t         top15\t         tiers 4, 5 & traders\t         stores\t         skyscrapers'
+	TIER3=$(wc -l < "${SPECIAL_FOLDER}/tier3.txt")
+	TIER4=$(wc -l < "${SPECIAL_FOLDER}/tier4.txt")
+	TIER5=$(wc -l < "${SPECIAL_FOLDER}/tier5.txt")
+	STORES=$(wc -l < "${SPECIAL_FOLDER}/stores.txt")
+	TOP7=$(wc -l < "${SPECIAL_FOLDER}/top7.txt")
+	TOP15=$(wc -l < "${SPECIAL_FOLDER}/top15.txt")
+	INDUSTRIAL=$(wc -l < "${SPECIAL_FOLDER}/industrial.txt")
+
+	printf "         tier 3 (%d)\t         tier 4 (%d)\t         tier 5 (%d)\t         stores (%d)\t         top %d\t         top %d\t         industrial (%d)\n" $TIER3 $TIER4 $TIER5 $STORES $TOP7 $TOP15 $INDUSTRIAL
 
 	# Hacks! I haven't figured out the best way of not hard coding this
 	paste \
+		<(SPECIAL=tier3.txt sortBySpecialPrefabs.sh) \
+		<(SPECIAL=tier4.txt sortBySpecialPrefabs.sh) \
+		<(SPECIAL=tier5.txt sortBySpecialPrefabs.sh) \
+		<(SPECIAL=stores.txt sortBySpecialPrefabs.sh) \
 		<(SPECIAL=top7.txt sortBySpecialPrefabs.sh) \
 		<(SPECIAL=top15.txt sortBySpecialPrefabs.sh) \
-		<(SPECIAL=special.txt sortBySpecialPrefabs.sh) \
-		<(SPECIAL=stores.txt sortBySpecialPrefabs.sh) \
-		<(SPECIAL=skyscrapers.txt sortBySpecialPrefabs.sh)
+		<(SPECIAL=industrial.txt sortBySpecialPrefabs.sh)
 
-	echo $'         top7\t         top15\t         tiers 4, 5 & traders\t         stores\t         skyscrapers'
+	printf "         tier 3 (%d)\t         tier 4 (%d)\t         tier 5 (%d)\t         stores (%d)\t         top %d\t         top %d\t         industrial (%d)\n" $TIER3 $TIER4 $TIER5 $STORES $TOP7 $TOP15 $INDUSTRIAL
 }
-
-#getSorts() {
-#	echo $'      # of Prefabs\t     Uniq. Int.\t     # of Int.\t     Unique'
-#	paste \
-#		<(sortByPrefabs.sh)  \
-#		<(sortBySpecialPrefabs.sh) \
-#		<(sortByTotalSpecialPrefabs.sh) \
-#		<(sortByUniquePrefabs.sh)
-#}
 
 if [[ -t 1 ]]; then
 	getSorts | column -t -s $'\t' | "$BIN"/highlight.sh "$@"
