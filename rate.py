f719e84f09cf9372ef7808bfe74f7f2ea7f08dbd
Daniel Sobral <dcsobral@gmail.com>
2021-08-02 12:57:32 -0600

    Break up rate into functions for ease of profiling

--- rate.py
+++ rate.py
@@ -75,17 +75,42 @@ bucket_range = range(start, end)
 
 special_folder = "%s/special" % bin
 special_prefabs = {}
-for special in specials:
-    with open("%s/%s.txt" % (special_folder, special)) as f:
-        special_prefabs[special] = [line.rstrip() for line in f]
-
 prefab_specials = {}
-for group in special_prefabs:
-    for prefab in special_prefabs[group]:
-        if prefab in prefab_specials:
-            prefab_specials[prefab].append(group)
-        else:
-            prefab_specials[prefab] = [group]
+bucket_specials = {}
+horizontal_aggregate = {}
+vertical_aggregate = {}
+max_score = 0.0
+max_bucket = ""
+max_position = "none"
+max_x = 0
+max_y = 0
+true_score = 0.0
+true_radius=sqrt(pow(diameter * precision / 2.0, 2) * 2)
+true_aggregate={special: set() for special in specials}
+
+def main():
+    load_specials()
+    set_prefab_specials()
+    set_bucket_specials()
+    compute_horizontal_aggregate()
+    compute_vertical_aggregate()
+    find_max()
+    compute_true_max()
+    print_score_breakdown()
+    print_score()
+
+def load_specials():
+    for special in specials:
+        with open("%s/%s.txt" % (special_folder, special)) as f:
+            special_prefabs[special] = [line.rstrip() for line in f]
+
+def set_prefab_specials():
+    for group in special_prefabs:
+        for prefab in special_prefabs[group]:
+            if prefab in prefab_specials:
+                prefab_specials[prefab].append(group)
+            else:
+                prefab_specials[prefab] = [group]
 
 
 def get_bucket(position):
@@ -94,99 +119,99 @@ def get_bucket(position):
     z = int(xyz[2])
     return "%d,%d" % (x / precision, z / precision)
 
-
-bucket_specials = {}
-for x in bucket_range:
-    for y in bucket_range:
-        bucket = "%d,%d" % (x, y)
-        bucket_specials[bucket] = {special: set() for special in specials}
-
-tree = ET.parse(prefabs_file)
-root = tree.getroot()
-prefabs = root.findall("decoration")
-for prefab in prefabs:
-    name = prefab.attrib["name"]
-    position = prefab.attrib["position"]
-    bucket = get_bucket(position)
-    if name in prefab_specials:
-        for special in prefab_specials[name]:
-            bucket_specials[bucket][special].add(name)
-
-horizontal_aggregate = {}
-for special in specials:
-    horizontal_aggregate[special] = {}
-    for y in bucket_range:
-        current = Counter()
-        for x in range(start - ahead, start) + bucket_range:
-            if x + ahead in bucket_range:
-                next = "%d,%d" % (x + ahead, y)
-                current.update(bucket_specials[next][special])
-
-            if x - backwards in bucket_range:
-                prev = "%d,%d" % (x - backwards, y)
-                current.subtract(bucket_specials[prev][special])
-
-            if x in bucket_range:
-                bucket = "%d,%d" % (x, y)
-                horizontal_aggregate[special][bucket] = current + Counter()
-
-vertical_aggregate = {}
-for special in specials:
-    vertical_aggregate[special] = {}
-    for x in bucket_range:
-        current = Counter()
-        for y in range(start - ahead, start) + bucket_range:
-            if y + ahead in bucket_range:
-                next = "%d,%d" % (x, y + ahead)
-                current.update(horizontal_aggregate[special][next])
-
-            if y - backwards in bucket_range:
-                prev = "%d,%d" % (x, y - backwards)
-                current.subtract(horizontal_aggregate[special][prev])
-
-            if y in bucket_range:
-                bucket = "%d,%d" % (x, y)
-                vertical_aggregate[special][bucket] = current + Counter()
-
-score = {}
-max_score = 0.0
-max_bucket = ""
-max_position = "none"
-max_x = 0
-max_y = 0
-offset = precision if diameter % 2 == 0 else precision / 2
-for x in bucket_range:
-    for y in bucket_range:
-        bucket = "%d,%d" % (x, y)
-        score[bucket] = 1.0
-        for special in specials:
-            score[bucket] = score[bucket] * \
-                len(vertical_aggregate[special][bucket]
-                    ) / len(special_prefabs[special])
-        if score[bucket] > max_score:
-            max_score = score[bucket]
-            max_bucket = bucket
-            max_x = x * precision + offset
-            max_y = y * precision + offset
-            max_position = "%d,%d" % (max_x, max_y)
-
-if debug:
+def set_bucket_specials():
+    global prefabs
     for x in bucket_range:
         for y in bucket_range:
             bucket = "%d,%d" % (x, y)
-            if [
-                    vertical_aggregate[special][bucket] for special in specials
-                    if vertical_aggregate[special][bucket]
-            ]:
-                print("%s %f: " % (bucket, score[bucket]), end='')
-                for special in specials:
-                    print("%s %d/%d/%d/%d " %
-                          (special, len(bucket_specials[bucket][special]),
-                           len(horizontal_aggregate[special][bucket]),
-                           len(vertical_aggregate[special][bucket]),
-                           len(special_prefabs[special])),
-                          end='')
-                print()
+            bucket_specials[bucket] = {special: set() for special in specials}
+    tree = ET.parse(prefabs_file)
+    root = tree.getroot()
+    prefabs = root.findall("decoration")
+    for prefab in prefabs:
+        name = prefab.attrib["name"]
+        position = prefab.attrib["position"]
+        bucket = get_bucket(position)
+        if name in prefab_specials:
+            for special in prefab_specials[name]:
+                bucket_specials[bucket][special].add(name)
+
+def compute_horizontal_aggregate():
+    for special in specials:
+        horizontal_aggregate[special] = {}
+        for y in bucket_range:
+            current = Counter()
+            for x in range(start - ahead, start) + bucket_range:
+                if x + ahead in bucket_range:
+                    next = "%d,%d" % (x + ahead, y)
+                    current.update(bucket_specials[next][special])
+
+                if x - backwards in bucket_range:
+                    prev = "%d,%d" % (x - backwards, y)
+                    current.subtract(bucket_specials[prev][special])
+
+                if x in bucket_range:
+                    bucket = "%d,%d" % (x, y)
+                    horizontal_aggregate[special][bucket] = current + Counter()
+
+def compute_vertical_aggregate():
+    for special in specials:
+        vertical_aggregate[special] = {}
+        for x in bucket_range:
+            current = Counter()
+            for y in range(start - ahead, start) + bucket_range:
+                if y + ahead in bucket_range:
+                    next = "%d,%d" % (x, y + ahead)
+                    current.update(horizontal_aggregate[special][next])
+
+                if y - backwards in bucket_range:
+                    prev = "%d,%d" % (x, y - backwards)
+                    current.subtract(horizontal_aggregate[special][prev])
+
+                if y in bucket_range:
+                    bucket = "%d,%d" % (x, y)
+                    vertical_aggregate[special][bucket] = current + Counter()
+
+def find_max():
+    global max_score
+    global max_bucket
+    global max_x
+    global max_y
+    global max_position
+    score = {}
+    offset = precision if diameter % 2 == 0 else precision / 2
+    for x in bucket_range:
+        for y in bucket_range:
+            bucket = "%d,%d" % (x, y)
+            score[bucket] = 1.0
+            for special in specials:
+                score[bucket] = score[bucket] * \
+                    len(vertical_aggregate[special][bucket]
+                        ) / len(special_prefabs[special])
+            if score[bucket] > max_score:
+                max_score = score[bucket]
+                max_bucket = bucket
+                max_x = x * precision + offset
+                max_y = y * precision + offset
+                max_position = "%d,%d" % (max_x, max_y)
+
+    if debug:
+        for x in bucket_range:
+            for y in bucket_range:
+                bucket = "%d,%d" % (x, y)
+                if [
+                        vertical_aggregate[special][bucket] for special in specials
+                        if vertical_aggregate[special][bucket]
+                ]:
+                    print("%s %f: " % (bucket, score[bucket]), end='')
+                    for special in specials:
+                        print("%s %d/%d/%d/%d " %
+                              (special, len(bucket_specials[bucket][special]),
+                               len(horizontal_aggregate[special][bucket]),
+                               len(vertical_aggregate[special][bucket]),
+                               len(special_prefabs[special])),
+                              end='')
+                    print()
 
 
 def distance(x1, y1, x2, y2):
@@ -199,41 +224,44 @@ def get_coord(position):
     z = int(xyz[2])
     return (x, z)
 
+def compute_true_max():
+    global true_score
+    if max_position != "none":
+        for prefab in prefabs:
+            name = prefab.attrib["name"]
+            position = prefab.attrib["position"]
+            x, y = get_coord(position)
+            if distance(x, y, max_x, max_y) <= true_radius:
+                if name in prefab_specials:
+                    for special in prefab_specials[name]:
+                        true_aggregate[special].add(name)
+        true_score = 1.0
+        for special in specials:
+            true_score = true_score * len(true_aggregate[special]) / len(special_prefabs[special])
 
-if max_position != "none":
-    true_radius=sqrt(pow(diameter * precision / 2.0, 2) * 2)
-    true_aggregate={special: set() for special in specials}
-    for prefab in prefabs:
-        name = prefab.attrib["name"]
-        position = prefab.attrib["position"]
-        x, y = get_coord(position)
-        if distance(x, y, max_x, max_y) <= true_radius:
-            if name in prefab_specials:
-                for special in prefab_specials[name]:
-                    true_aggregate[special].add(name)
-    true_score = 1.0
-    for special in specials:
-        true_score = true_score * len(true_aggregate[special]) / len(special_prefabs[special])
-else:
-    true_score = 0.0
+def print_score_breakdown():
+    if verbose and max_position != "none":
+        for special in specials:
+    #        print("%s (%d/%d):\t" %
+    #              (special, len(vertical_aggregate[special][max_bucket]),
+    #               len(special_prefabs[special])),
+    #              end='')
+    #        for prefab in sorted(vertical_aggregate[special][max_bucket]):
+    #            print("%s " % prefab, end='')
+    #        print()
+            print("%s (%d/%d):\t" %
+                  (special, len(true_aggregate[special]),
+                   len(special_prefabs[special])),
+                  end='')
+            for prefab in sorted(true_aggregate[special]):
+                print("%s " % prefab, end='')
+            print()
 
-if verbose and max_position != "none":
-    for special in specials:
-#        print("%s (%d/%d):\t" %
-#              (special, len(vertical_aggregate[special][max_bucket]),
-#               len(special_prefabs[special])),
-#              end='')
-#        for prefab in sorted(vertical_aggregate[special][max_bucket]):
-#            print("%s " % prefab, end='')
-#        print()
-        print("%s (%d/%d):\t" %
-              (special, len(true_aggregate[special]),
-               len(special_prefabs[special])),
-              end='')
-        for prefab in sorted(true_aggregate[special]):
-            print("%s " % prefab, end='')
-        print()
+def print_score():
+    #print("%d %d %s" % (true_score * 1000000, max_score * 1000000, max_position))
+    print("%d %s" % (true_score * 1000000, max_position))
 
-#print("%d %d %s" % (true_score * 1000000, max_score * 1000000, max_position))
-print("%d %s" % (true_score * 1000000, max_position))
+if __name__ == '__main__':
+    main()
+
 
