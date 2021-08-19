b9bf24b7cac036411fb234807774188d3bcd4b8e
Daniel Sobral <dcsobral@gmail.com>
2021-08-19 14:31:20 -0600

    Switch to new tag-based zoning system

--- prefabZoningColor.sh
+++ prefabZoningColor.sh
@@ -10,77 +10,129 @@ if [[ $# -eq 0 ]]; then
         exit 1
 fi
 
+declare -a zones
+
+# TODO: make zones an associative array instead?
+hasZone() {
+	local IFS='|'
+	PATTERN="($*)"
+	IFS=$','
+	if [[ ",${zones[*]}," =~ ,${PATTERN}, ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
 for prefab; do
-	if [[ $prefab == *trader* ]]; then
-		echo magenta
-		exit 0
-	elif [[ $prefab == *field* ]]; then
-		echo olive
-		exit 0
+	zones=()
+
+	if [[ $prefab == *field* ]]; then
+		zones+=(crop)
 	fi
 
         if [[ "$prefab" =~ rwg_tile* ]]; then
-		case "$prefab" in
-			rwg_tile_citycenter* | rwg_tile_downtown*)
-				zones="downtown"
+		prefabZone="${prefab#rwg_tile_}"
+		if [[ $prefabZone == wasteland* ]]; then
+			zones+=(wasteland)
+			prefabZone="${prefabZone#wasteland}"
+		fi
+		case "${prefabZone}" in
+			citycenter*)
+				zones+=(citycenter)
 				;;
-			rwg_tile_commercial*)
-				zones="commercial"
+			downtown*)
+				zones+=(downtown)
 				;;
-			rwg_tile_countryresidential* | rwg_tile_countrytown* | rwg_tile_rural*)
-				zones="residentialold"
+			commercial*)
+				zones+=(commercial)
 				;;
-			rwg_tile_residential*)
-				zones="residentialold,residentialnew"
+			countryresidential*)
+				zones+=(countryresidential)
 				;;
-			rwg_tile_ghosttown* | rwg_tile_oldwest*)
-				zones="oldwest"
+			countrytown*)
+				zones+=(countrytown)
 				;;
-			rwg_tile_industrial*)
-				zones="industrial"
+			rural*)
+				zones+=(rural)
+				;;
+			residential*)
+				zones+=(residential)
+				;;
+			ghosttown* | oldwest*)
+				zones+=(oldwest)
+				;;
+			industrial*)
+				zones+=(industrial)
 				;;
 			*)
-				zones="any"
+				zones+=(any)
 				;;
 		esac
-		if [[ -z $zones ]]; then
-			FILE="${PREFABS}/RWGTiles/${prefab}.xml"
-		fi
+		FILE="${PREFABS}/RWGTiles/${prefab}.xml"
 	else
-		zones=""
 		FILE="${PREFABS}/POIs/${prefab}.xml"
 	fi
 
-	if [[ -z "${zones}" ]]; then
-		[[ -f "${FILE}" ]] || FILE="$(find "${PREFABS}" -name "${prefab}.xml" -print)"
-		zones="$(xmlstarlet sel -t -m /prefab -v "property[@name='Zoning']/@value" \
-			"${FILE}" | tr '[:upper:]' '[:lower:]' || :)"
-	fi
+	[[ -f "${FILE}" ]] || FILE="$(find "${PREFABS}" -name "${prefab}.xml" -print)"
+	mapfile -d ' ' -O ${#zones[@]} -t zones < <(xmlstarlet sel -t -m /prefab -v "property[@name='Tags']/@value" \
+		"${FILE}" | tr '[:upper:],' '[:lower:] ' || :)
 
-	if [[ $zones == *any* ]]; then
+	# Existing tags:
+	# citycenter x
+	# commercial x
+	# countryresidential x
+	# countrytown x -- smaller than city and town
+	# downtown x
+	# industrial x
+	# oldwest x
+	# residential x
+	# rural x -- outskirt of cities and towns (but not countrytown)
+	# test
+	# trader x
+	# wastelandcommercial x
+	# wastelandcountryresidential x
+	# wastelandcountrytown x
+	# wastelanddowntown x
+	# wastelandindustrial x
+	# wastelandresidential x
+	# wilderness x
+
+	# Preempt any other zones
+	if hasZone trader; then
+		echo magenta
+		continue
+	elif hasZone crop; then
+		echo olive
+		continue
+	elif hasZone any; then
 		echo white
-		exit 0
-	elif [[ $zones == *nozone* || $zones == *oldwest* ]]; then
+		continue
+	elif hasZone oldwest; then
 		echo saddlebrown
-		exit 0
+		continue
 	fi
 
-	[[ "$zones" == *residentialold* ]] && RESIDENTIAL_OLD=1 || RESIDENTIAL_OLD=""
-	[[ "$zones" == *residentialnew* ]] && RESIDENTIAL_NEW=1 || RESIDENTIAL_NEW=""
-	[[ "$zones" == *industrial* ]] && INDUSTRIAL=1 || INDUSTRIAL=""
-	[[ "$zones" == *commercial* ]] && COMMERCIAL=1 || COMMERCIAL=""
-	[[ "$zones" == *downtown* ]] && DOWNTOWN=1 || DOWNTOWN=""
+	# Zones used in combos for special colors
+	hasZone residential && RESIDENTIAL_NEW=1 || RESIDENTIAL_NEW=""
+	hasZone countryresidential && RESIDENTIAL_OLD=1 || RESIDENTIAL_OLD=""
+	hasZone residential countryresidential && RESIDENTIAL=1 || RESIDENTIAL=""
+	hasZone industrial && INDUSTRIAL=1 || INDUSTRIAL=""
+	hasZone commercial && COMMERCIAL=1 || COMMERCIAL=""
+	hasZone downtown citycenter && DOWNTOWN=1 || DOWNTOWN=""
 
-	[[ -n $RESIDENTIAL_OLD || -n $RESIDENTIAL_NEW ]] && RESIDENTIAL=1 || RESIDENTIAL=""
-
+	# First match wins
 	if [[ -n $DOWNTOWN && -n $COMMERCIAL && -n $INDUSTRIAL ]]; then echo orange
 	elif [[ -n $DOWNTOWN && -n $RESIDENTIAL ]]; then echo teal
 	elif [[ -n $DOWNTOWN && -n $COMMERCIAL ]]; then echo slategray
 	elif [[ -n $DOWNTOWN ]]; then echo gray
-	elif [[ -n $RESIDENTIAL_OLD ]]; then echo green
 	elif [[ -n $RESIDENTIAL_NEW ]]; then echo chartreuse
+	elif [[ -n $RESIDENTIAL_OLD ]]; then echo green
 	elif [[ -n $COMMERCIAL ]]; then echo blue
 	elif [[ -n $INDUSTRIAL ]]; then echo yellow
+	elif hasZone rural; then echo olivedrab
+	elif hasZone countrytown; then echo sandybrown
+	elif hasZone wilderness; then echo saddlebrown
 	else echo black
 	fi
 done
