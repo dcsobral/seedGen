2b15f8bd52521ca1aa48e4debaaab3f0c5c8490c
Daniel Sobral <dcsobral@gmail.com>
2021-09-09 13:36:47 -0600

    Make showPrefab work on all subfolders

--- showPrefab.sh
+++ showPrefab.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\t\n'
 
 : "${F7D2D:?Please export F7D2D with 7D2D install folder}"
-PREFABS="${F7D2D}/Data/Prefabs/POIs"
+PREFABS="${F7D2D}/Data/Prefabs"
 
 if [[ $# -eq 0 ]]; then
 	echo >&2 "$0 <prefab>"
@@ -11,8 +11,8 @@ if [[ $# -eq 0 ]]; then
 fi
 
 
-
 for prefab; do
-	cmd.exe /C start "" "$(wslpath -w "$PREFABS/$prefab.jpg")"
+	PREFAB="$(find "${PREFABS}" -name "${prefab}.jpg" -print)"
+	cmd.exe /C start "" "$(wslpath -w "${PREFAB}")"
 done
 
