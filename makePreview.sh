4825801c7cde7ce2006d4fb39b77eff77ab01a13
Daniel Sobral <dcsobral@gmail.com>
2024-07-26 10:40:21 -0600

    Add sanity checks for makePreview.sh.
    If map_info.xml doesn't have the seed name -- such as in the case of the
    pregen maps -- then the folder name will be used instead.

--- makePreview.sh
+++ makePreview.sh
@@ -2,8 +2,11 @@
 
 BIN="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+[[ -f map_info.xml ]] || echo >&2 "No map_info.xml file found at $PWD!"
+
 SIZE="$(xmlstarlet sel -t -m "/MapInfo/property[@name='HeightMapSize']/@value" -v . map_info.xml | cut -d , -f 1)"
 SEED="$(xmlstarlet sel -t -m "/MapInfo/property[@name='Generation.Seed']/@value" -v . map_info.xml)"
+[[ -n $SEED ]] || SEED="$(basename "$PWD")"
 
 IMG="$("${BIN}/drawMap.sh" ${SIZE} "${SEED}")"
 
